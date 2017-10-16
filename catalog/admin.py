4cc3f4b0a9aece148183ef70ab672ec031e43ed7
realnica <realnica@hotmail.com>
2017-10-16 09:58:51 -0600

    First version of application moved into github

---
+++ catalog/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+#admin.site.register(Book)
+# Register the Admin classes for Book using the decorator
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+admin.site.register(Genre)
+
+#admin.site.register(BookInstance)
+# Register the Admin classes for BookInstance using the decorator
+
+
+
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'borrower','due_back', 'id')
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+
+
+#admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
